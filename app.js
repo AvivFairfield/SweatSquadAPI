cdec7aa040a10dde166d4cd4eea71e4d43d18282
Aviv Cohen <avivcohen22@icloud.com>
2024-03-24 12:59:56 +0200

    added doc

--- app.js
+++ app.js
@@ -1,38 +1,47 @@
-const express = require("express");
-const cors = require("cors");
-const jwt = require("jsonwebtoken");
-const app = express();
-const port = process.env.PORT || 5000;
+//This server application is built using the Express framework to manage backend operations
+//for a fitness application named "SweatSquad". It facilitates user authentication,
+//workout session management, and user preferences updates through various endpoints.
+//The application manages data interactions through a PostgreSQL database.
 
-const secretjwt = "baguette";
+//Importing necessary libraries
+const express = require("express"); //Express framework
+const cors = require("cors"); //Middleware to enable CORS (Cross-Origin Resource Sharing)
+const jwt = require("jsonwebtoken"); //Library to work with JSON Web Tokens for authentication
+const app = express(); //Creating an instance of express
+const port = process.env.PORT || 5000; //Setting the port number, either from environment variables or default to 5000
 
-// Middleware to parse JSON bodies
+const secretjwt = "baguette"; //Defining a secret key for JWT
+
+//Middleware to parse JSON bodies
+//This allows us to easily access request body data
 app.use(express.json());
 
-// Use Database
+//Importing the database connection pool
 const { pool } = require("./db");
 
+//CORS options to allow requests from the specified origin
 var corsOptions = {
 	origin: "http://localhost:5000",
-	optionsSuccessStatus: 200, // For legacy browser support
+	optionsSuccessStatus: 200, //For legacy browser support
 };
 
 app.use(cors());
 
+//Test route to check if the server is running and can connect to the backend
 app.get("/test", async (req, res) => {
 	res.send("Test Worked, Connected to the backend.");
 });
 
 // app.post("/get-training-data")
 
+//Route to handle user login
 app.post("/login", async (req, res) => {
 	try {
-		const { email, password } = req.body;
+		const { email, password } = req.body; //Extracting email and password from request body
 
 		console.log(email);
 
-		// validate all parameters that are exist and valid
-
+		//Checking if both email and password were provided
 		if (!email || !password) {
 			res.json({
 				message: "You must provide an email & password",
@@ -41,16 +50,16 @@ app.post("/login", async (req, res) => {
 			return;
 		}
 
-		// Look up user in the database and password
+		//Querying the database for a user that matches the provided email and password
 		const { rows } = await pool.query(
 			"SELECT * FROM public.users WHERE email = $1 and password = $2",
 			[email, password]
 		);
 
-		// Check if a user was found
+		//Check if a user was found
 		const userExists = rows.length > 0;
 
-		const user = rows[0];
+		const user = rows[0]; //The found user
 
 		if (!userExists) {
 			res.json({
@@ -59,23 +68,23 @@ app.post("/login", async (req, res) => {
 			});
 			return;
 		}
-
+		//Generating a JWT for the user
 		const token = jwt.sign(
 			{
-				id: user.id,
+				id: user.id, //Including user details in the token
 				email: user.email,
 				first_name: user.first_name,
 				last_name: user.last_name,
 				password: user.password,
 			},
-			secretjwt,
+			secretjwt, //Secret key for signing the token
 			{
-				expiresIn: "8h",
+				expiresIn: "8h", //Token expires in 8 hours
 			}
 		);
 
 		console.log(`token: ${token}`);
-
+		//Responding with the generated token and user details
 		res.json({
 			status: "success",
 			token: token,
@@ -96,16 +105,18 @@ app.post("/login", async (req, res) => {
 	}
 });
 
+//Route to verify the authenticity of a JWT
 app.post("/api/auth/verify", async (req, res) => {
 	try {
 		const { token } = req.body;
-
+		//Checking if token was provided
 		if (!token || token === "") {
 			return res.status(401).json({ message: "Token is missing" });
 		}
 
-		// Verify JWT token
+		//Verify JWT token
 		const decoded = jwt.verify(token, secretjwt);
+		//Responding with the decoded token details
 		res.status(200).json({
 			status: "success",
 			message: `Token is valid for ${
@@ -125,13 +136,14 @@ app.post("/api/auth/verify", async (req, res) => {
 	}
 });
 
+//Route for saving a workout session
 app.post("/saveworkout", async (req, res) => {
 	try {
 		const { email, trainingType, location, duration, workoutDate } =
 			req.body;
 
 		console.log(email, trainingType, location, duration, workoutDate);
-
+		//sends a SQL command to the db to insert the extracted workout session details into the public.active_workouts table.
 		await pool.query(
 			`
 			INSERT INTO public.active_workouts(
@@ -157,11 +169,12 @@ app.post("/saveworkout", async (req, res) => {
 	}
 });
 
+//Route to retrieve all workouts from the 'workout_list' table.
 app.get("/getallworkouts", async (req, res) => {
 	try {
 		console.log("Getting all workouts");
 
-		// insert query
+		//insert query
 		const allWorkouts = await pool.query(
 			`
 			SELECT workout_name FROM public.workout_list;`
@@ -182,7 +195,7 @@ app.get("/getallworkouts", async (req, res) => {
 		});
 	}
 });
-
+//Route to fetch workout history for a user based on their email
 app.post("/getuserhistory", async (req, res) => {
 	try {
 		const { email } = req.body;
@@ -209,7 +222,7 @@ app.post("/getuserhistory", async (req, res) => {
 		});
 	}
 });
-
+//Route for fetching active workouts for a user
 app.post("/getuserworkouts", async (req, res) => {
 	try {
 		const { email } = req.body;
@@ -229,14 +242,14 @@ app.post("/getuserworkouts", async (req, res) => {
 			workouts: userWorkouts.rows,
 		});
 	} catch (getuserworkouts_err) {
-		// console.log(err);
+		//console.log(err);
 		res.json({
 			status: "failed",
 			message: `Invalid input err:${getuserworkouts_err}`,
 		});
 	}
 });
-
+//Route to delete a specific workout session for a user by ID
 app.post("/deleteuserworkouts", async (req, res) => {
 	try {
 		const { id } = req.body;
@@ -261,7 +274,7 @@ app.post("/deleteuserworkouts", async (req, res) => {
 		});
 	}
 });
-
+//Route to archive (move to history) a workout session for a user
 app.post("/archiveuserworkouts", async (req, res) => {
 	try {
 		const { id } = req.body;
@@ -291,9 +304,10 @@ WHERE id=${formattedId}`);
 		});
 	}
 });
-
+//Route to register a new user in the system
 app.post("/registeruser", async (req, res) => {
 	try {
+		//Extract user details from the request body
 		const { email } = req.body;
 		await pool.query(
 			`
@@ -333,11 +347,12 @@ app.post("/registeruser", async (req, res) => {
 		});
 	}
 });
-
+//Route to change a user's password
 app.post("/changepassword", async (req, res) => {
 	try {
+		//Extract the email, old password, and new password from the request body
 		const { email, oldpw, newpw } = req.body;
-
+		//Retrieve the current password for the user from the database
 		const { rows } = await pool.query(
 			`
 			SELECT password
@@ -345,7 +360,7 @@ app.post("/changepassword", async (req, res) => {
 			[email]
 		);
 		const userPass = rows[0].password;
-
+		//Check if the old password matches the current password
 		if (userPass != oldpw) {
 			res.json({
 				message: "Invalid password",
@@ -353,7 +368,7 @@ app.post("/changepassword", async (req, res) => {
 			});
 			return;
 		}
-
+		//Update the user's password in the database to the new password
 		await pool.query(
 			`
 			UPDATE public.users
@@ -373,18 +388,18 @@ app.post("/changepassword", async (req, res) => {
 		});
 	}
 });
-
+//Route to retrieve or update account settings for a user
 app.post("/accountsettings", async (req, res) => {
 	try {
 		const { email } = req.body;
-
+		//Retrieve the user's current settings from the database
 		const { rows } = await pool.query(
 			`
 			SELECT weight, goal,height
 	FROM public.users WHERE email='${email}';`
 		);
 		userData = rows[0];
-
+		//Respond with the retrieved user data
 		res.status(200).json({
 			status: "success",
 			message: "Retrived info successfuly",
@@ -393,14 +408,14 @@ app.post("/accountsettings", async (req, res) => {
 			userWeight: userData.weight,
 		});
 	} catch (err) {
-		// console.log(err);
+		//console.log(err);
 		res.json({
 			status: "failed",
 			message: `Invalid input err:${err}`,
 		});
 	}
 });
-
+//Starts the server and listens on the specified port for requests
 app.listen(port, () => {
 	console.log(`SweatSquad listening at :${port}`);
 });
--- db.js
+++ db.js
@@ -1,10 +1,14 @@
+//This file configures the database connection for the application
+//using the pg module, specifically utilizing connection pooling
+//for efficient management of database connections
+
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-	user: "postgres",
+	user: "postgres", //Credentials for database access
 	password: "ss123",
-	database: "postgres",
-	host: "176.230.127.84",
+	database: "postgres", //Specifies which database to connect to within PostgreSQL.
+	host: "176.230.127.84", //The network address and port where the PostgreSQL server is accessible
 	port: 5432,
 });
 
